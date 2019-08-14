edb5f5eeae1dd2842728342e4f227a00a92d1f3e
Diptis Halder <diptis.halder@gmail.com>
2019-08-14 10:44:56 +0100

    final test: 51NGAP0RE corrected to SINGAPORE

--- correct.test.js
+++ correct.test.js
@@ -7,3 +7,7 @@ test('L0ND0N is corrected to LONDON', () => {
 test('DUBL1N is corrected to DUBLIN', () => {
   expect(correct('DUBL1N')).toBe('DUBLIN');
 });
+
+test('51NGAP0RE is corrected to SINGAPORE', () => {
+  expect(correct('51NGAP0RE')).toBe('SINGAPORE');
+});
